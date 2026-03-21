e089d6564ae3a6eaa6dbc2bf87c77f18e309fd78
bellocqcatalina <bellocqcatalina@gmail.com>
2026-03-21 13:38:29 -0300

    Primer Cambio: control de entradas no válidas

--- questions.py
+++ questions.py
@@ -37,6 +37,10 @@ while attempts > 0:
     print(f"Letras usadas: {', '.join(guessed)}")
     letter = input("Ingresá una letra: ")
 
+    if len(letter) !=1 or not letter.isalpha():
+        print("Entrada no válida")
+        continue
+
     if letter in guessed:
         print("Ya usaste esa letra.")
     elif letter in word:
